bfc61975a750e70f1fc992bfa17eed77c16374f0
Jennifermax123 <72615865+Jennifermax123@users.noreply.github.com>
2023-09-27 12:12:55 +0300

    Fifth Commit - MySQL Cnx - Models - Menu API - Booking Table API' ;

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Menu(models.Model):
+    title =  models.CharField(max_length=255, default='')
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    inventory = models.IntegerField(default=0)
+
+
+class Booking(models.Model):
+    name = models.CharField(max_length=255)
+    no_of_guests = models.IntegerField()
+    bookingDate = models.DateTimeField()
